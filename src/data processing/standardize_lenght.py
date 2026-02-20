905538482a23e138bccfb86c874e085739f19ee8
FHpopova21 <FHPopova21@codingburgas.bg>
2026-02-20 17:45:20 +0200

    feat: Implement new audio data processing utilities, an AudioCNN model, and refactor source directory structure.

---
+++ src/data processing/standardize_lenght.py
@@ -0,0 +1,35 @@
+import os
+import glob
+import librosa
+import numpy as np 
+import librosa.display
+from sklearn.preprocessing import LabelEncoder
+
+def standardize_lenght(y, sr, targer_duration = 2.0 ):
+    """
+    Standardize the length of an audio file.
+
+    Args:
+        y (np.ndarray): Audio time series.
+        sr (int): Sample rate.
+        targer_duration (float): Target duration in seconds. Default is 2.0.
+
+    Returns:
+        np.ndarray: Standardized audio time series.
+        int: Sample rate.
+    """
+    
+    target_lenght = int(targer_duration * sr)
+    current_lenght = len(y)
+
+    if current_lenght < target_lenght:
+        # Zero-Padding (Допълване с нули равномерно от двете страни)
+
+        padding_lenght = target_lenght - current_lenght
+        padding_left = padding_lenght // 2
+        padding_right = padding_lenght - padding_left
+
+        y = np.pad(y, (padding_left, padding_right), mode = "constant")
+
+    elif current_lenght > target_lenght:
+        #

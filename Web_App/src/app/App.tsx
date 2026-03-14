5ec85958e63e5e3f128b8b470575af2a984f3782
FHpopova21 <FHPopova21@codingburgas.bg>
2026-03-14 15:14:11 +0200

    chore: Add `//.` comment to App.tsx.

--- Web_App/src/app/App.tsx
+++ Web_App/src/app/App.tsx
@@ -5,7 +5,7 @@ import { HowItWorksSection } from "./components/how-it-works-section";
 import { DemoSection } from "./components/demo-section";
 import { TechStackSection } from "./components/tech-stack-section";
 import { Footer } from "./components/footer";
-
+//.
 export default function App() {
   return (
     <div className="min-h-screen bg-[#0f0620] text-white overflow-x-hidden">
